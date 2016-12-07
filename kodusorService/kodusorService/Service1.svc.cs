9aeb339218c709628d76135563a3063acc1de3b0
kadirsefaunal <kadir-unal@outlook.com>
2016-12-08 00:03:33 +0300

    Proje dosyaları düzenlendi.

---
+++ kodusorService/kodusorService/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using kodusorService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace kodusorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string sefa()
+        {
+            kodusorEntities e = new kodusorEntities();
+            Kullanicilar k = new Kullanicilar();
+            k.Adi = "szs";
+            k.DogumTarihi = DateTime.Now;
+            k.Parola = "123";
+            k.Mail = "as@as.com";
+            k.Soyadi = "sd";
+            e.Kullanicilar.Add(k);
+            e.SaveChanges();
+            return "kayır ";
+        }
+    }
+}
